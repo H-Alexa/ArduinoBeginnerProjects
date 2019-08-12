aa3a0bc6047f2814c60ccf11e8a3d4b08a9ef45b
H-Alexa <54031572+H-Alexa@users.noreply.github.com>
2019-08-12 20:05:39 +0600

    Add files via upload

---
+++ blinking_led/blinking_led.ino
@@ -0,0 +1,15 @@
+int led = 9;
+void setup() {
+  pinMode(led,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(led,HIGH);
+  delay(500);
+  digitalWrite(led,LOW);
+  delay(500);
+  // put your main code here, to run repeatedly:
+
+}
